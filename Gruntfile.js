32059c81f4587b08fb707feb73fd66b414ebdb64
juno5460 <lyuno@vip.qq.com>
2013-08-03 14:07:51 +0800

    add grunt-bower-task
    grunt-bower-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,28 @@ module.exports = function(grunt) {
 		stylus: {
 			compile: {
 				files: {
-					'dest/css/piece.css': 'src/stylus/piece.styl' 
+					'dest/css/piece.css': 'src/stylus/piece.styl'
 				}
 			}
-		}
+		},
+		bower: {
+			install: {
+				//just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
+				options: {
+					targetDir: './vendor',
+					layout: 'byComponent',
+					install: true,
+					verbose: true,
+					cleanTargetDir: true,
+					cleanBowerDir: true
+				}
+			}
+		},
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-bower-task');
 
-	grunt.registerTask('default', ['stylus']);
+	grunt.registerTask('default', ['stylus', 'bower']);
 };
