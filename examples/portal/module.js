f5e8119c9e3777f138fdb6ddd4f09bfb0ae647ce
juno5460 <lyuno@vip.qq.com>
2013-12-18 18:04:43 +0800

    update css
    update css

--- examples/portal/module.js
+++ examples/portal/module.js
@@ -1,9 +1,8 @@
 define(function(require) {
-	var v1 = require('portal/PortalView');
-	var v2 = require('portal/detailView');
-
-	return {
-		'PortalView': v1,
-		'detailView': v2
-	};
+     var v1 = require('portal/PortalView');
+     var v2 = require('portal/detailView');
+     return {
+         'PortalView': v1,
+         'detailView': v2
+      }
 });
